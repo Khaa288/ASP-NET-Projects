e5f87aeefc2b08f9d5cb2a3aa57f15e990cf0174
Shinoooooooooo <89777765+Khaa288@users.noreply.github.com>
2023-07-22 13:39:23 +0700

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Practice_ASP_NET.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
