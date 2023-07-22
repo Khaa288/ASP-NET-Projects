9d6ff90cae4b3fa0b34ac78cbf0b62f2405297fd
Shinoooooooooo <89777765+Khaa288@users.noreply.github.com>
2023-07-22 15:46:04 +0700

    Add DbContext and Services into Program

--- Program.cs
+++ Program.cs
@@ -2,6 +2,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<AppDbContext>();
+builder.Services.AddTransient<DbContextServices>();
 
 var app = builder.Build();
 
